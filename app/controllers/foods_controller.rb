4a1ab49623479cb3b1cac6b5a98c344f4de79b60
AmanWorku <amexworku2014@gmail.com>
2023-06-28 11:37:01 +0300

    Add Index view for the foods section

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,44 +1,46 @@
-class FoodsController < ApplicationController 
-    before_action :set_food, only: %i[show edit update destroy]
-
-  def index
-    @foods = current_user.foods
-  end
-
-  def show
-    @food = Food.find(params[:id])
-  end
-
-  def new
-    @food = Food.new
-  end
-  def create
-    @food = current_user.foods.build(food_params)
-
-    if @food.save
-      redirect_to foods_path(@food, current_user), notice: 'Food successfully added!'
-    else
-      render 'new', notice: 'Something went wrong!'
+# app/controllers/foods_controller.rb
+class FoodsController < ApplicationController
+    before_action :authenticate_user!
+  
+    def index
+      @foods = current_user.foods.all
+    end
+  
+    def new
+      @food = current_user.foods.build
+    end
+  
+    def create
+      @food = current_user.foods.build(food_params)
+  
+      if @food.save
+        redirect_to foods_path
+      else
+        render :new
+      end
+    end
+  
+    def show
+      @food = current_user.foods.find(params[:id])
+    end
+  
+    def edit
+      @food = current_user.foods.find(params[:id])
+    end
+  
+    def destroy
+      @food = current_user.foods.find(params[:id])
+      @food.destroy
+      redirect_to foods_path
+    end
+  
+    def my_foods
+      @foods = current_user.foods.all
+    end
+  
+    private
+  
+    def food_params
+      params.require(:food).permit(:name, :measurement_unit, :price, :quantity)
     end
   end
-
-  @food = Food.find(params[:id])
-  @food.destroy
-
-  if @food&.destroy
-    redirect_to foods_path, notice: 'Food successfully deleted!'
-  else
-    render 'new', notice: 'Something went wrong!'
-  end
-end
-
-  private
-
-  def set_food
-    @food = Food.find(params[:id])
-  end
-
-  def food_params
-    params.require(:food).permit(:name, :measurement_unit, :unit_price, :quantity, :user_id)
-  end
-end
