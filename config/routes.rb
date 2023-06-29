4ed24f3449c234dd7a2188fa9501c9ed53b2a708
AmanWorku <amexworku2014@gmail.com>
2023-06-29 15:29:46 +0300

    Refactor recipefood model

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,8 @@ Rails.application.routes.draw do
   
   
   resources :recipes do
-    resources :recipe_foods. only: [:new, :create, :destroy, :show, :edit, :update] 
+    resources :recipe_foods, only: [:new, :create, :destroy, :show, :edit, :update]
   end
-
   resources :foods, only: [:index, :show, :new, :create, :destroy]
   resources :public_recipes, only: [:index]
   get '/shopping_list', to: 'shopping_lists#index', as: 'shopping_list'
